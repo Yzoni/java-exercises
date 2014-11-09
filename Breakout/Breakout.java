27dd825dd8b7c1fda486d43f621a9ab06c9a1434
Yorick <yzonix@gmail.com>
2014-11-09 17:56:55 +0100

    homework 3 pre

---
+++ Breakout/Breakout.java
@@ -0,0 +1,145 @@
+/*
+ * File: Breakout.java
+ * -------------------
+ * Name:
+ * Section Leader:
+ * 
+ * This file will eventually implement the game of Breakout.
+ */
+
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Breakout extends GraphicsProgram
+{
+	/** Width and height of application window in pixels */
+	public static final int APPLICATION_WIDTH = 400;
+	public static final int APPLICATION_HEIGHT = 600;
+
+	/** Dimensions of game board (usually the same) */
+	private static final int WIDTH = APPLICATION_WIDTH;
+	private static final int HEIGHT = APPLICATION_HEIGHT;
+
+	/** Dimensions of the paddle */
+	private static final int PADDLE_WIDTH = 60;
+	private static final int PADDLE_HEIGHT = 10;
+
+	/** Offset of the paddle up from the bottom */
+	private static final int PADDLE_Y_OFFSET = 30;
+
+	/** Number of bricks per row */
+	private static final int NBRICKS_PER_ROW = 10;
+
+	/** Number of rows of bricks */
+	private static final int NBRICK_ROWS = 10;
+
+	/** Separation between bricks */
+	private static final int BRICK_SEP = 4;
+
+	/** Width of a brick */
+	private static final int BRICK_WIDTH =
+		(WIDTH - (NBRICKS_PER_ROW - 1) * BRICK_SEP) / NBRICKS_PER_ROW;
+
+	/** Height of a brick */
+	private static final int BRICK_HEIGHT = 8;
+
+	/** Radius of the ball in pixels */
+	private static final int BALL_RADIUS = 10;
+
+	/** Offset of the top brick row from the top */
+	private static final int BRICK_Y_OFFSET = 70;
+
+	/** Number of turns */
+	private static final int NTURNS = 3;
+
+	/** Runs the Breakout program. */
+	public void run()
+	{
+		setupBricks();
+		setupPaddle();
+		addMouseListeners();
+
+		createBall();
+
+		waitForClick();
+
+		moveBall();
+	}
+
+	private void setupBricks() {
+		for (int i = 0; i < NBRICK_ROWS; i++) {
+			int y = BRICK_Y_OFFSET + (BRICK_HEIGHT + BRICK_SEP) * i;
+			//Draw a layer of bricks
+			for (int j = 0; j <= NBRICKS_PER_ROW; j++) {
+				int x= (j - 1) * (BRICK_WIDTH + BRICK_SEP); 
+				GRect brick = new GRect(x, y, BRICK_WIDTH, BRICK_HEIGHT);
+				brick.setFilled(true);
+				if (i == 0 || i == 1) {
+					brick.setColor(Color.red);
+				} else if (i == 2 || i == 3) {
+					brick.setColor(Color.orange);
+				} else if (i == 4 || i == 5) {
+					brick.setColor(Color.yellow);
+				} else if (i == 6 || i == 7) {
+					brick.setColor(Color.green);
+				} else if (i == 8 || i == 9) {
+					brick.setColor(Color.cyan);
+				}
+				add(brick);
+			}
+		}
+	}
+
+	private void setupPaddle() {
+		int x = 0;
+		int y = HEIGHT - (PADDLE_HEIGHT + PADDLE_Y_OFFSET);
+		paddle = new GRect(x, y, PADDLE_WIDTH, PADDLE_HEIGHT);
+		paddle.setFilled(true);
+		paddle.setColor(Color.black);
+		add(paddle);
+	} 
+
+	public void mouseMoved(MouseEvent e) {
+		if (e.getX() < (WIDTH - PADDLE_WIDTH)) {
+			paddle.move(e.getX() - paddle.getX(), 0);
+		}
+	}
+
+	private void createBall() {
+		ball = new GOval((WIDTH - BALL_RADIUS) /2, (HEIGHT - BALL_RADIUS)/ 2, BALL_RADIUS, BALL_RADIUS);
+		ball.setFilled(true);
+		ball.setColor(Color.black);
+		add(ball);
+	}
+
+	private void moveBall() {
+		vx = rgen.nextDouble(1.0, 3.0);
+		vy = rgen.nextDouble(1.0, 3.0);
+		if (rgen.nextBoolean(0.5))
+    		vx = -vx;
+		if (ball.getX() == 0 || ball.getX() == WIDTH - BALL_RADIUS) {
+    			vx = -vx;
+		}
+		if (ball.getY() == 0 || ball.getY() == HEIGHT - BALL_RADIUS) {
+    			vy = -vy;
+    	}
+
+    	while (true) {
+			ball.move(vx, vy);
+			pause(10);
+		}
+	}
+
+	private GRect paddle;
+	private GOval ball;
+
+	// Velocity of the ball
+	private double vx, vy;
+
+	// Random number generator
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+ }
